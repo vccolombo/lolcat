df314aa4b6c7904e8573d70efa9c8d9b20efa600
Victor Cora Colombo <victorcora98@gmail.com>
2021-09-24 21:39:02 -0300

    File reading is now done by chunks

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"math"
 	"os"
 )
@@ -16,55 +15,66 @@ func rgb(i int) (int, int, int) {
 		int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
 }
 
-func readFile(path string) (string, error) {
-	// TODO: relative path
-	content, err := ioutil.ReadFile(path)
-	if err != nil {
-		return "", err
-	}
-
-	return string(content), nil
-}
-
 func printRainbowChar(c rune, rainbowSeed int) {
 	r, g, b := rgb(rainbowSeed)
 	fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, c)
 }
 
+var rainbowSeed int = 0
+
 func displayContents(content string) {
 	for i := 0; i < len(content); i++ {
-		printRainbowChar(rune(content[i]), i)
+		printRainbowChar(rune(content[i]), rainbowSeed)
+		rainbowSeed += 1
+	}
+}
+
+func displayChunksFromFile(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	defer file.Close()
+
+	const chunkSz = 16
+	buf := make([]byte, chunkSz)
+
+	for {
+		readTotal, err := file.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Fprintln(os.Stderr, err)
+			}
+			break
+		}
+
+		chunk := string(buf[:readTotal])
+
+		displayContents(chunk)
 	}
 }
 
 func runWithFiles() {
 	nArgs := len(os.Args)
 	if nArgs <= 1 {
-		fmt.Printf("usage: %s [filename1] [filename2] [...]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "usage: %s [filename1] [filename2] [...]\n", os.Args[0])
 		os.Exit(1)
 	}
 
 	for _, file := range os.Args[1:] {
-		content, err := readFile(file)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		displayContents(content)
+		displayChunksFromFile(file)
 	}
 }
 
 func runWithPipe() {
 	reader := bufio.NewReader(os.Stdin)
-	i := 0
 	for {
 		input, _, err := reader.ReadRune()
 		if err != nil && err == io.EOF {
 			break
 		}
-		printRainbowChar(input, i)
-		i += 1
+		displayContents(string(input))
 	}
 }
 
